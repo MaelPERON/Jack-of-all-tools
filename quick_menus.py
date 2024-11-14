bc1fd3fff99ebbf5a52b84c04b7804625dce8e4f
MaelPERON <maelperonmessagerie2015@gmail.com>
2024-11-14 16:25:28 +0100

    Add quick pie menu

--- quick_menus.py
+++ quick_menus.py
@@ -1,6 +1,8 @@
 import bpy
 from math import radians
 
+from_quickmenu = False
+
 def menu_merge(self, context):
     layout = self.layout
     layout.separator()
@@ -14,6 +16,17 @@ def menu_uv(self, context):
         op = layout.operator("mesh.edges_select_sharp",text=f'{angle}°',icon="IPO_CONSTANT")
         op.sharpness = radians(angle-1)
 
+class QuickMenu(bpy.types.Menu):
+    bl_label = "Quick Menu"
+    bl_idname = "VIEW3D_MT_joat_quickmenu"
+
+    def draw(self, context):
+        layout = self.layout.menu_pie()
+        obj = context.object
+        layout.label(text="Hello World")
+        layout.prop(obj, "name")
+        global from_quickmenu
+        from_quickmenu = True
 
 def register():
     bpy.types.VIEW3D_MT_edit_mesh_merge.append(menu_merge)
