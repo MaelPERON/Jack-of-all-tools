e8ef8eec6d09e1324059a49c5968a6f4a3951ca9
MaelPERON <maelperonmessagerie2015@gmail.com>
2024-11-19 14:47:03 +0100

    Create placeOperator utils function
    It create a shortway for adding an operator button to a layout (adding an icon if found)

--- utils.py
+++ utils.py
@@ -1,2 +1,8 @@
 def getIndex(list, index):
     return list[index] if index < len(list) else None
+
+def placeOperator(layout, Operator):
+    kwargs = {}
+    if (icon := getattr(Operator, "bl_icon", None)):
+        kwargs['icon'] = icon
+    layout.operator(Operator.bl_idname, text=Operator.bl_label, **kwargs)
