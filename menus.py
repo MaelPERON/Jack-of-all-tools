77fd5731f3924e408d629e922dd79435553f7218
MaelPERON <maelperonmessagerie2015@gmail.com>
2024-11-20 14:19:26 +0100

    Added SelectObjectWithModifiers operator to select object menu

--- menus.py
+++ menus.py
@@ -1,16 +1,27 @@
 import bpy
 from .utils import placeOperator
-from .operators import SummonBone
+from .operators import SummonBone, SelectObjectWithModifiers
 
 def SummonBoneOperator(self, context):
     layout = self.layout
     layout.separator()
     placeOperator(layout, SummonBone)
 
+def SelectObjectWithModifiersOperator(self, context):
+    layout = self.layout
+    layout.separator()
+    placeOperator(layout, SelectObjectWithModifiers)
+
 def register():
+    # Context Menu
     bpy.types.VIEW3D_MT_edit_mesh_context_menu.append(SummonBoneOperator)
     bpy.types.VIEW3D_MT_mesh_add.append(SummonBoneOperator)
+    # Select object menu
+    bpy.types.VIEW3D_MT_select_object.append(SelectObjectWithModifiersOperator)
 
 def unregister():
+    # Context Menu
     bpy.types.VIEW3D_MT_edit_mesh_context_menu.remove(SummonBoneOperator)
     bpy.types.VIEW3D_MT_mesh_add.remove(SummonBoneOperator)
+    # Select object menu
+    bpy.types.VIEW3D_MT_select_object.remove(SelectObjectWithModifiersOperator)
